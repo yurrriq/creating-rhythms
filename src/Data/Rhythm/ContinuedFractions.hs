2023d58438e6dfab9c31a96461abe83fb44f7712
Eric Bailey <eric@ericb.me>
2024-12-22 23:22:12 -0600

    feat(haskell): port cfsqrt

--- src/Data/Rhythm/ContinuedFractions.hs
+++ src/Data/Rhythm/ContinuedFractions.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Data.Rhythm.ContinuedFractions where
 
+import Control.Monad (guard)
 import Data.Bifunctor (first)
 import Data.Functor.Base (NonEmptyF (..))
-import Data.Functor.Foldable (cata)
+import Data.Functor.Foldable (ana, cata)
 import Data.List (intercalate)
 import Data.List.NonEmpty (NonEmpty (..))
 import Data.Maybe (listToMaybe)
+import Data.Tuple.Extra (thd3)
 
 -- | A 'ContinuedFraction' is a 'NonEmpty', potentially infinite, list of
 -- integer 'coefficients'.
@@ -30,3 +34,26 @@ collapseFraction = cata (algebra . first toRational) . coefficients
   where
     algebra (NonEmptyF i Nothing) = i
     algebra (NonEmptyF i (Just d)) = i + recip d
+
+-- | Calculate the 'ContinuedFraction' representation of the square root of a
+-- given number.
+continuedFractionSqrt :: (Integral a) => a -> ContinuedFraction
+continuedFractionSqrt n
+  | root * root < n =
+      ContinuedFraction $ fromIntegral . thd3 <$> ana coalgebra (0, 1, root)
+  | otherwise =
+      ContinuedFraction (fromIntegral root :| [])
+  where
+    coalgebra (a, b, c) =
+      NonEmptyF (a, b, c) $
+        do
+          guard (c /= 2 * root)
+          let a' = b * c - a
+          let b' = (n' - a' * a') `div` b
+          let c' = (root + a') `div` b'
+          pure (a', b', c')
+    root = isqrt n
+    n' = fromIntegral n
+
+isqrt :: (Integral a) => a -> a
+isqrt = truncate . sqrt @Double . fromIntegral
