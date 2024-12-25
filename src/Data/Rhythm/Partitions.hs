dd70cbd5961ca167a7f4389fe2c9c4e0b7493d66
Eric Bailey <eric@ericb.me>
2024-12-25 00:46:03 -0600

    feat(haskell): port partam

--- src/Data/Rhythm/Partitions.hs
+++ src/Data/Rhythm/Partitions.hs
@@ -2,6 +2,7 @@ module Data.Rhythm.Partitions
   ( partitions,
     partitionsAllowed,
     partitionsLength,
+    partitionsLengthAllowed,
   )
 where
 
@@ -26,3 +27,11 @@ partitionsAllowed allowed n =
 -- [Partition [3,2,2],Partition [3,3,1],Partition [4,2,1],Partition [5,1,1]]
 partitionsLength :: Int -> Int -> [Partition]
 partitionsLength = partitionsWithKParts
+
+-- | foo bar baz
+--
+-- >>> partitionsLengthAllowed 2 [1,2,3] 4
+-- [Partition [2,2],Partition [3,1]]
+partitionsLengthAllowed :: (Foldable t) => Int -> t Int -> Int -> [Partition]
+partitionsLengthAllowed len allowed =
+  filter (all (`elem` allowed) . fromPartition) . partitionsLength len
