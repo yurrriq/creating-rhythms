98b729a8cca0cee251b2d426e3bb02049ec19184
Eric Bailey <eric@ericb.me>
2024-12-25 00:25:15 -0600

    feat(haskell): port partm

--- src/Data/Rhythm/Partitions.hs
+++ src/Data/Rhythm/Partitions.hs
@@ -1,6 +1,14 @@
 module Data.Rhythm.Partitions
   ( partitions,
+    partitionsLength,
   )
 where
 
-import Math.Combinat.Partitions (partitions)
+import Math.Combinat.Partitions (Partition, partitions, partitionsWithKParts)
+
+-- | Partitions of a given length.
+--
+-- >>> partitionsLength 3 7
+-- [Partition [3,2,2],Partition [3,3,1],Partition [4,2,1],Partition [5,1,1]]
+partitionsLength :: Int -> Int -> [Partition]
+partitionsLength = partitionsWithKParts
