cf3a438d56ad17ba36af78d3b936648ff7f4b0a4
Eric Bailey <eric@ericb.me>
2024-12-25 00:38:46 -0600

    feat(haskell): port parta

--- src/Data/Rhythm/Partitions.hs
+++ src/Data/Rhythm/Partitions.hs
@@ -1,10 +1,24 @@
 module Data.Rhythm.Partitions
   ( partitions,
+    partitionsAllowed,
     partitionsLength,
   )
 where
 
-import Math.Combinat.Partitions (Partition, partitions, partitionsWithKParts)
+import Math.Combinat.Partitions
+  ( Partition,
+    fromPartition,
+    partitions,
+    partitionsWithKParts,
+  )
+
+-- | Partitions with allowed parts.
+--
+-- >>> partitionsAllowed [1,2,3] 4
+-- [Partition [1,1,1,1],Partition [2,1,1],Partition [2,2],Partition [3,1]]
+partitionsAllowed :: (Foldable t) => t Int -> Int -> [Partition]
+partitionsAllowed allowed n =
+  filter (all (`elem` allowed) . fromPartition) (partitions n)
 
 -- | Partitions of a given length.
 --
