d2af2e6290a490153307826fa5f229317b4dfb15
Eric Bailey <eric@ericb.me>
2025-01-26 01:25:58 -0600

    refactor(haskell): use Data.Tree in necklacesRSW

--- src/Data/Rhythm/Binary.hs
+++ src/Data/Rhythm/Binary.hs
@@ -7,6 +7,7 @@ import Data.FastDigits (digits)
 import Data.List (sortOn)
 import Data.List.Extra (splitOn)
 import Data.Ord (Down (..))
+import Data.Tree (Tree (..), flatten, unfoldTree)
 
 -- | Convert a binary string to a list of intervals.
 --
@@ -32,29 +33,37 @@ necklaces :: Int -> [[Int]]
 necklaces n =
   sortOn Down $
     map (take n . (++ repeat 0) . digits 2) $
-      necklacesRSW n
+      flatten (necklacesRSW n)
 
 -- | All binary necklaces of a given length, encoded as numbers.
 --
--- >>> necklacesRSW 4
+-- \[
+--   \begin{align*}
+--     \sigma(x_1 ... x_n) &= x_2 ... x_n x_1 \\
+--     \tau(x_1 ... x_{n-1}) &= x_1 ... x_{n-1}\overline{x_n}
+--   \end{align*}
+-- \]
+--
+-- Generate the tree of binary necklaces of length \(n\), starting with
+-- \(x = 0^n\) as root, where children of \(x\) are the necklaces of the form
+-- \(\tau\sigma^j(x)\) for \(1 \le j \le n -1\).
+--
+-- >>> flatten (necklacesRSW 4)
 -- [0,1,3,7,15,5]
-necklacesRSW :: (Integral a, Bits a) => Int -> [a]
-necklacesRSW 0 = [0]
-necklacesRSW n = 0 : search 1
+necklacesRSW :: (Integral a, Bits a) => Int -> Tree a
+necklacesRSW 0 = Node 0 []
+necklacesRSW n = Node 0 [unfoldTree search 1]
   where
     -- rotation
     σ necklace = rotateL necklace 1 `mod` m
     σʲ = take n . iterate σ
 
-    -- negate least significant bit
     τ = flip complementBit 0
 
-    -- Find n-ary binary necklaces
+    -- build the tree of find n-ary binary necklaces with a given root
     search necklace
-      | necklace == m = [necklace]
-      | otherwise = necklace : concatMap search candidates
-      where
-        candidates = takeWhile isNecklace (map τ (tail (σʲ necklace)))
+      | necklace == m = (necklace, [])
+      | otherwise = (necklace, takeWhile isNecklace (map τ (tail (σʲ necklace))))
 
     -- a necklace is the lexicographically smallest rotation
     isNecklace necklace =
