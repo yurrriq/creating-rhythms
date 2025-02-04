4a7fc48ae1eb8daff42935b5ed68e5e2c9f95555
Eric Bailey <eric@ericb.me>
2025-02-04 01:36:13 -0600

    feat(cfcv): add Haskell exe

---
+++ app/cfcv/Main.hs
@@ -0,0 +1,23 @@
+import Control.Arrow ((&&&))
+import qualified Data.List.NonEmpty as NE
+import Data.Ratio (denominator, numerator)
+import Data.Rhythm.ContinuedFractions (ContinuedFraction (..), collapseFraction)
+import Options.Applicative
+import Text.Printf (printf)
+
+main :: IO ()
+main = printFraction =<< customExecParser p opts
+  where
+    opts =
+      info
+        (some (argument auto (metavar "TERMS...")))
+        (fullDesc <> progDesc "Calculate a continued fraction convergent.")
+    p = prefs showHelpOnEmpty
+
+printFraction :: [Integer] -> IO ()
+printFraction =
+  uncurry (printf "%d %d\n")
+    . (numerator &&& denominator)
+    . Data.Rhythm.ContinuedFractions.collapseFraction
+    . ContinuedFraction
+    . NE.fromList
