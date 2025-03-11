ffa740397c0863eed707ffc63e6b265daf253664
Eric Bailey <eric@ericb.me>
2025-03-10 23:57:05 -0500

    feat(cfsqrt): add Haskell exe

---
+++ app/cfsqrt/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TypeApplications #-}
+
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.Rhythm.ContinuedFractions (ContinuedFraction (..), continuedFractionSqrt)
+import Options.Applicative
+import Text.Printf (printf)
+
+main :: IO ()
+main =
+  putStrLn
+    . (\(n :| ds) -> printf "%d ( %s )" n (unwords (map show ds)))
+    . coefficients
+    . continuedFractionSqrt @Integer
+    =<< customExecParser p opts
+  where
+    opts =
+      info (argument auto (metavar "N")) $
+        progDesc
+          "Calculate the continued fraction representation of sqrt(N).\n\
+          \The periodic part is in parentheses."
+    p = prefs showHelpOnEmpty
