6c670343a9b7b1dfd672a5e933b877bddb7b4974
Eric Bailey <eric@ericb.me>
2025-03-10 22:03:24 -0500

    feat(compm): add Haskell exe

---
+++ app/compm/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TypeApplications #-}
+
+import Data.Rhythm.Compositions (compositionsLength)
+import Options.Applicative
+
+main :: IO ()
+main =
+  putStr
+    . unlines
+    . map (unwords . map show)
+    . uncurry (flip (compositionsLength @Integer))
+    =<< customExecParser p opts
+  where
+    opts =
+      info
+        ( (,)
+            <$> argument auto (metavar "N")
+            <*> argument auto (metavar "M")
+        )
+        (fullDesc <> progDesc "Positive compositions of a given length.")
+    p = prefs showHelpOnEmpty
