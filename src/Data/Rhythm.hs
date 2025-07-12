960ee07914fa616e3392329d9b8be60d0ef69090
Eric Bailey <eric@ericb.me>
2025-07-12 11:50:09 -0500

    feat: add Data.Rhythm

---
+++ src/Data/Rhythm.hs
@@ -0,0 +1,31 @@
+-- |
+-- Module      : Data.Rhythm.Binary
+-- Copyright   : (c) Eric Bailey, 2025
+--
+-- License     : MIT
+-- Maintainer  : eric@ericb.me
+-- Stability   : stable
+-- Portability : POSIX
+--
+-- Algorithms from [Creating Rhythms](https://abrazol.com/books/rhythm1/)
+-- by Stefan Hollos and J. Richard Hollos.
+module Data.Rhythm
+  ( module Data.Rhythm.Binary,
+    module Data.Rhythm.Christoffel,
+    module Data.Rhythm.Compositions,
+    module Data.Rhythm.ContinuedFractions,
+    module Data.Rhythm.Markov,
+    module Data.Rhythm.Partitions,
+    module Data.Rhythm.Permutations,
+    module Data.Rhythm.Random,
+  )
+where
+
+import Data.Rhythm.Binary
+import Data.Rhythm.Christoffel
+import Data.Rhythm.Compositions
+import Data.Rhythm.ContinuedFractions
+import Data.Rhythm.Markov
+import Data.Rhythm.Partitions
+import Data.Rhythm.Permutations
+import Data.Rhythm.Random
